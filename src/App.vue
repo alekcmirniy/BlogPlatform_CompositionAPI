1f18dad9e7b5645670cf02a04def690cc18058bd
alekcmirniy <alekcmirniy@bk.ru>
2025-11-07 18:23:44 +0300

    background gradient added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import NavigationComponent from './components/NavigationComponent.vue';
-import { Repeater } from './tasks/Repeater';
 
-Repeater.createInstance(1000, () => console.log('Выполнение функции...'));
 
 </script>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,15 +3,11 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import PostsView from '@/views/PostsView.vue'
-import RepeaterView from '@/views/RepeaterView.vue'
-import DirectoryView from '@/views/DirectoryView.vue'
 
 const routes: RouteRecordRaw[] = [
   { path: "/", name: "Главная", component: HomeView },
   { path: "/login", name: "Вход", component: LoginView },
   { path: "/posts", name: "Публикации", component: PostsView },
-  { path: "/repeater", name: "Repeater", component: RepeaterView },
-  { path: "/directory", name: "DirectoryClasses", component: DirectoryView }
 ]
 
 const router = createRouter({
